f47bfd70184c8f722871ac100cfd077d8d7d1012
Soprodevento <Sashinka2803@gmail.com>
2024-04-21 14:45:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        LazyStudent student = new LazyStudent();
+        student.study();
+    }
+
+    public static class Student {
+        protected final String studying;
+
+        protected Student(String work) {
+            this.studying = work;
+        }
+
+        public Student() {
+            this.studying = "Прохожу тестовое задание.";
+        }
+
+        public void study() {
+            System.out.println("Я очень занят. " + studying);
+        }
+    }
+    public static class LazyStudent extends Student{
+        @Override
+        public void study(){
+            super.study();
+            System.out.println("Сегодня не учусь, мне лень.");
+        }
+    }
+}
